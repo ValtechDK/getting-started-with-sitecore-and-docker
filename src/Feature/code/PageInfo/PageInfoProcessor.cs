28bc9b33589c323a5a78280c7cb6520c93882112
Per Manniche Bering <pmbering@gmail.com>
2020-03-30 14:48:23 +0200

    adding some code to demo debugging

---
+++ src/Feature/code/PageInfo/PageInfoProcessor.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Feature.PageInfo
+{
+    public class PageInfoProcessor : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (args.Aborted || Context.Item == null)
+            {
+                return;
+            }
+
+            args.HttpContext.Response.Headers["X-Sitecore-Item"] = Context.Item.Uri.ToString();
+        }
+    }
+}
